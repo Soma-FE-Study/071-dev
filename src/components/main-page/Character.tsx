d688ed9fd2d1c97d8b56034bbcf49d0525ae946b
071yoon <yeonggilol@gmail.com>
2022-05-07 00:16:56 +0900

    ✨ 캐릭터의 움직임을 구현! 귀여운 뚜벅이가 움직입니다

--- src/components/main-page/Character.tsx
+++ src/components/main-page/Character.tsx
@@ -4,49 +4,91 @@ import styled from 'styled-components';
 
 export default function Character() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  console.log(position);
+  const [img, setImg] = useState('/img/character-standing-high.png');
+  const [dir, setDir] = useState(0); //0 -> stand, 1 -> right, 2 -> left
+  const [imgCnt, setImgCnt] = useState(0); // 0, 1, 2, 3
 
   const containerRef = useRef<HTMLDivElement | null>(null);
   let heightMax = useRef(0);
   let widthMax = useRef(0);
+
   const keyHandler = (event: KeyboardEvent) => {
     event.preventDefault();
     const hMAX = heightMax.current;
     const wMAX = widthMax.current;
+
     if (event.key === 'ArrowRight') {
-      console.log('right');
       if (position.x >= wMAX - 100) {
         return;
       }
-      setPosition({ ...position, x: position.x + 15 });
+
+      if (dir === 1) {
+        setImgCnt((imgCnt + 1) % 4);
+      } else {
+        setDir(1);
+        setImgCnt(0);
+      }
+      setImg(`/img/character-right-${imgCnt}-high.png`);
+
+      setPosition({ ...position, x: position.x + 20 });
     } else if (event.key === 'ArrowLeft') {
-      console.log('left');
       if (position.x <= 10) {
-        console.log('out of x');
         return;
       }
-      setPosition({ ...position, x: position.x - 15 });
+
+      if (dir === 2) {
+        setImgCnt((imgCnt + 1) % 4);
+      } else {
+        setDir(2);
+        setImgCnt(0);
+      }
+      setImg(`/img/character-left-${imgCnt}-high.png`);
+
+      setPosition({ ...position, x: position.x - 20 });
     } else if (event.key === 'ArrowDown') {
-      console.log('down');
       if (position.y >= hMAX - 130) {
         return;
       }
-      setPosition({ ...position, y: position.y + 15 });
+
+      if (dir === 0) {
+        setDir(1);
+        setImgCnt(-1);
+      }
+      setImgCnt((imgCnt + 1) % 4);
+      if (dir === 1) {
+        setImg(`/img/character-right-${imgCnt}-high.png`);
+      } else {
+        setImg(`/img/character-left-${imgCnt}-high.png`);
+      }
+
+      setPosition({ ...position, y: position.y + 20 });
     } else if (event.key === 'ArrowUp') {
-      console.log('up');
       if (position.y <= 10) {
-        console.log('out of y');
         return;
       }
-      setPosition({ ...position, y: position.y - 15 });
+
+      if (dir === 0) {
+        setDir(1);
+        setImgCnt(-1);
+      }
+      setImgCnt((imgCnt + 1) % 4);
+      if (dir === 1) {
+        setImg(`/img/character-right-${imgCnt}-high.png`);
+      } else {
+        setImg(`/img/character-left-${imgCnt}-high.png`);
+      }
+
+      setPosition({ ...position, y: position.y - 20 });
     }
   };
+
   useEffect(() => {
     window.addEventListener('keydown', keyHandler);
     return () => {
       window.removeEventListener('keydown', keyHandler);
     };
   });
+
   useEffect(() => {
     if (containerRef.current !== null) {
       heightMax.current = containerRef.current.clientHeight;
@@ -54,10 +96,11 @@ export default function Character() {
       setPosition({ x: widthMax.current / 2 - 50, y: heightMax.current / 2 - 50 });
     }
   }, []);
+
   return (
     <Container ref={containerRef}>
       <CharacterContainer style={{ left: `${position.x}px`, top: `${position.y}px` }}>
-        <Image className='char' src='/img/character-standing-high.png' width='100' height='100' />
+        <Image src={img} width='100' height='100' />
       </CharacterContainer>
     </Container>
   );
