0fddc9b8c2e1ab0223e8fe0077c48fb10d829675
071yoon <yeonggilol@gmail.com>
2022-05-26 20:21:59 +0900

    ⚡️ 이미지 돌아가게 설정해서 blink 문제 간접적으로 해결

--- src/components/main-page/Background.tsx
+++ src/components/main-page/Background.tsx
@@ -1,26 +1,43 @@
-import { useRef, useEffect, ReactElement } from 'react';
-import { CanvasHTMLAttributes } from 'react';
+import { useState, ReactElement } from 'react';
+import Image from 'next/image';
 import styled from 'styled-components';
+import useInterval from '../../Hooks/useInterval';
 
 export default function Background({ children }: { children: ReactElement }) {
-  //   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [leftImg, setLeft] = useState('/img/character-left');
+  const [rightImg, setRight] = useState('/img/character-right');
+  const [imgCnt, setImgCnt] = useState(0);
 
-  //   useEffect(() => {
-  //     if (!canvasRef.current) return;
-  //     const ctx = canvasRef.current.getContext('2d');
-  //     const image = new Image();
-  //     image.src = 'img/background.png';
-  //     image.onload = function () {
-  //       if (!ctx) return;
-  //       ctx.drawImage(image, 0, 0);
-  //     };
-  //   }, [canvasRef]);
+  useInterval(() => {
+    setImgCnt((imgCnt + 1) % 4);
+    setLeft(`/img/character-left-${imgCnt}.png`);
+    setRight(`/img/character-right-${imgCnt}.png`);
+  }, 200);
+
   return (
     <BackgroundCanvas id='top' className='background'>
+      <LeftCharacter>
+        <Image src={rightImg} alt='character' width='40' height='40' />
+      </LeftCharacter>
+      <RightCharacter>
+        <Image src={leftImg} alt='character' width='40' height='40' />
+      </RightCharacter>
       {children}
     </BackgroundCanvas>
   );
 }
+
+const RightCharacter = styled.div`
+  right: 1.5rem;
+  position: absolute;
+  image-rendering: crisp-edges;
+`;
+
+const LeftCharacter = styled.div`
+  position: absolute;
+  left: 1.5rem;
+  image-rendering: crisp-edges;
+`;
 
 const BackgroundCanvas = styled.div`
   background-image: url('/img/background.png');
